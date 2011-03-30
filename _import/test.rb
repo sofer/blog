5bc33943738a94c662dcb56b1edb06d5802e9756
D Sofer <d@sofer.com>
2011-03-30 23:38:41 +0100

    import from sofer.com

--- _import/test.rb
+++ _import/test.rb
@@ -1,3 +1,28 @@
-text = 'dffdsf "dfgdfg dfsdfs":http://sdfsdf.com dfsdf ds'
+#!/usr/bin/ruby
 
-puts text.gsub!(/"([^"]+)":(\S+)/, '[\1](\2)')
+#puts Dir['*.txt']
+
+#puts Dir.getwd
+indir = File.dirname(File.expand_path($0))
+outdir = indir.sub(/\w+$/, '_posts')
+
+puts indir
+puts outdir
+
+puts Dir.new(indir).select { |f| f =~ /\.txt$/ }
+
+
+
+__END__
+
+
+dir = '/Users/ds/code/sofer.github.com/_posts/'
+
+
+Dir.foreach(dir) { |f|
+  puts f
+}
+
+Dir.select(dir) { |f|
+  puts f
+}
---
+++ _import/textile-import.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/ruby
+
+# convert the blog folder to jekyll
+# start one file at a time
+
+$indir = File.dirname(File.expand_path($0))
+$outdir = $indir.sub(/\w+$/, '_posts')
+
+# special treatement for first line in form title|date|tags
+def split_first_line(line)
+  title, date, tags = line.split('|')
+  tags = tags.strip.split(',')
+  return title, date, tags
+end
+
+# alternatives and additions to textile
+def convert_copy(text)
+  #text.gsub!(/<\/?cite>/, '"')
+  #text.gsub!(/<\/?sup>/, '')
+  #text.gsub!(/"([^"]+)":(\S+\w)/, '[\1](\2)')
+  #text.gsub!('p(quote). ', '')
+  #text.gsub!('<p class="quote">', '')
+  #text.gsub!(/("[^"]+")/, '_\1_')
+  #text.gsub!(/^(\[\d+\])/, '\1:')
+  #text.gsub!('--', '–')
+  #text.gsub!('’', "'")
+  return text
+end
+
+
+def strip_standfirst(text)
+  text.gsub!(/\[\d+\]/, '')
+  text.gsub!(/"([^"]+)":\S+/, '\1')
+  return text
+end
+
+def convert_to_textile(filename)
+  text = File.read($indir+'/'+filename)
+  first_line, text = text.split("\n", 2)
+  title, date, tags = split_first_line(first_line)
+  text = convert_copy(text).strip
+  standfirst, ignore = text.split("\n",2)  
+  standfirst = strip_standfirst(standfirst)
+  out = <<END
+---
+layout: post
+title: #{title}
+category: #{tags}
+standfirst: #{standfirst}
+---  
+#{text}
+END
+  outfilename = date + '-' + filename.sub(/\.txt$/, ".textile")
+  outfilepath = $outdir+'/'+outfilename
+  outfile = File.open(outfilepath, 'w')
+  outfile << out
+  outfile.close
+  puts "processed #{outfilepath}"
+end
+
+files = Dir.new($indir).select { |f| f =~ /\.txt$/ }
+files.each { |fn|
+  convert_to_textile(fn)
+}
+
+
